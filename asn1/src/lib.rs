217a107cb2f275b9ebd6c261a9e466d9a9b9df69
meh <meh@1aim.com>
2019-04-11 13:42:37 +0200

    fix: use core::Value in to_writer and to_der

--- asn1/src/lib.rs
+++ asn1/src/lib.rs
@@ -15,17 +15,17 @@ pub use impl_der as der;
 use std::io;
 
 #[inline]
-pub fn to_writer<W, E, T>(writer: &mut W, mut encoder: E, value: T) -> Result<()>
-	where W: io::Write, E: core::Encoder + core::Encode<T>
+pub fn to_writer<W, E, T, V>(writer: &mut W, mut encoder: E, value: V) -> Result<()>
+	where W: io::Write, E: Encoder + Encode<T>, V: Into<Value<T>>
 {
-	encoder.encode(writer, value)?;
+	encoder.encode(writer, value.into())?;
 	Ok(())
 }
 
 #[cfg(feature = "der")]
 #[inline]
-pub fn to_der<W, T>(writer: &mut W, value: T) -> Result<()>
-	where W: io::Write, impl_der::Encoder: Encode<T>
+pub fn to_der<W, T, V>(writer: &mut W, value: V) -> Result<()>
+	where W: io::Write, der::Encoder: Encode<T>, V: Into<Value<T>>
 {
-	to_writer(writer, impl_der::Encoder, value)
+	to_writer(writer, der::Encoder, value)
 }
