97f39e6ad9ed2eb265d79ab06a529cc768548b6e
Erin Power <xampprocky@gmail.com>
2019-04-15 12:42:10 +0200

    refactor: Changed from using Strings to structs for codegen

--- asn1-spec/src/codegen.rs
+++ asn1-spec/src/codegen.rs
@@ -1,11 +1,16 @@
+mod structs;
+
 use std::io::Write;
 use std::collections::HashSet;
 use std::fmt::Write as _;
 
 use crate::{ast::*, Result, semantics::SemanticChecker};
+use self::structs::*;
 
 pub trait Backend: Default {
     fn generate_type(&mut self, ty: &Type) -> Result<String>;
+    fn generate_value(&mut self, value: &Value) -> Result<String>;
+    fn generate_value_assignment(&mut self, name: &str, ty: &Type, value: &Value) -> Result<String>;
     fn generate_sequence(&mut self, name: &str, fields: &[ComponentType]) -> Result<String>;
     fn generate_builtin(&mut self, builtin: &BuiltinType) -> Result<String>;
     fn write_prelude<W: Write>(&self, writer: &mut W) -> Result<()>;
@@ -14,38 +19,40 @@ pub trait Backend: Default {
 
 #[derive(Default)]
 pub struct Rust {
-    structs: Vec<String>,
+    consts: Vec<String>,
+    structs: Vec<Struct>,
     prelude: HashSet<String>,
+    indentation: usize,
+}
+
+impl Rust {
+    fn generate_const(&mut self, public: bool, name: &str, ty: &str, value: &str) {
+        use heck::ShoutySnakeCase;
+        let visibility = if public { "pub" } else { "" };
+        self.consts.push(format!("{} const {}: {} = {};", visibility, name.to_shouty_snake_case(), ty, value));
+    }
 }
 
 impl Backend for Rust {
     /// As Rust doesn't allow you to have anonymous structs `generate_sequence` returns the name
     /// of the struct and stores the definition seperately.
     fn generate_sequence(&mut self, name: &str, fields: &[ComponentType]) -> Result<String> {
-        let mut output = String::new();
-
-        writeln!(output, "struct {name} {{", name=name)?;
+        let mut generated_struct = Struct::new(name);
 
         for field in fields {
             // Unwrap currently needed as i haven't created the simplified AST without
             // `ComponentsOf` yet.
             let (ty, optional, default) = field.as_type().unwrap();
+            let field = FieldBuilder::new(&**ty.name.as_ref().unwrap(), &*self.generate_type(&ty)?)
+                            .optional(*optional)
+                            .default_value(default.clone()
+                                                  .and_then(|v| self.generate_value(&v).ok()))
+                            .build();
 
-            let ty_output = self.generate_type(&ty)?;
-            let type_output = if *optional {
-                format!("Option<{}>", ty_output)
-            } else {
-                ty_output
-            };
-
-            write!(output, "{}", " ".repeat(4))?;
-
-            writeln!(output, "{}: {},", ty.name.as_ref().unwrap(), type_output)?;
+            generated_struct.add_field(field);
         }
 
-        writeln!(output, "}}")?;
-
-        self.structs.push(output);
+        self.structs.push(generated_struct);
 
         Ok(String::from(name))
     }
@@ -54,7 +61,10 @@ impl Backend for Rust {
         match ty.raw_type {
             RawType::Builtin(ref builtin) => self.generate_builtin(builtin),
             RawType::Referenced(ref reference) if reference.is_internal() => Ok(reference.item.clone()),
-            _ => Ok(String::from("UNIMPLEMENTED"))
+            ref raw => {
+                warn!("UNKNOWN TYPE: {:?}", raw);
+                Ok(String::from("UNIMPLEMENTED"))
+            }
         }
     }
 
@@ -67,7 +77,10 @@ impl Backend for Rust {
             },
             BuiltinType::OctetString => String::from("Vec<u8>"),
             BuiltinType::Integer(_) => String::from("isize"),
-            _ => String::from("UNIMPLEMENTED"),
+            ref builtin => {
+                warn!("UNKNOWN BUILTIN TYPE: {:?}", builtin);
+                String::from("UNIMPLEMENTED")
+            }
         };
 
         Ok(output)
@@ -75,13 +88,33 @@ impl Backend for Rust {
 
     fn write_prelude<W: Write>(&self, writer: &mut W) -> Result<()> {
         writer.write(self.prelude.iter().cloned().collect::<Vec<_>>().join("\n").as_bytes())?;
+        writer.write(self.consts.iter().cloned().collect::<Vec<_>>().join("\n").as_bytes())?;
         Ok(())
     }
 
     fn write_footer<W: Write>(&self, writer: &mut W) -> Result<()> {
-        writer.write(self.structs.join("\n").as_bytes())?;
+        writer.write(itertools::join(self.structs.iter().map(ToString::to_string), "\n").as_bytes())?;
 
         Ok(())
+    }
+
+
+    fn generate_value(&mut self, value: &Value) -> Result<String> {
+        match value {
+            _ => Ok(String::from("UNIMPLEMENTED")),
+        }
+    }
+
+    fn generate_value_assignment(&mut self, name: &str, ty: &Type, value: &Value) -> Result<String> {
+
+        let generated_value = match value {
+            _ => String::from("UNIMPLEMENTED"),
+        };
+
+        let type_output = self.generate_type(ty)?;
+
+        self.generate_const(false, name, &type_output, &generated_value);
+        Ok(String::from(name))
     }
 }
 
@@ -101,6 +134,10 @@ impl<W: Write, B: Backend> CodeGenerator<W, B> {
     }
 
     pub fn generate(mut self) -> Result<W> {
+        for (name, (ty, value)) in self.table.values.iter() {
+            self.backend.generate_value_assignment(name, ty, value)?;
+        }
+
         for (name, ty) in self.table.types.iter() {
             match &ty.raw_type {
                 RawType::Builtin(BuiltinType::Sequence(components)) => {
