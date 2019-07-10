09def1ecfeaa77e9a50e5cae2c5ff7d6df5d0d83
Erin Power <e.power@dac.eu>
2019-07-10 12:58:21 +0200

    Added documentation for ObjectIdentifier

--- asn1-core/src/types/object_identifier.rs
+++ asn1-core/src/types/object_identifier.rs
@@ -1,6 +1,31 @@
 use failure::{ensure, Fallible};
 use serde::{Deserialize, Serialize};
 
+/// A representation of the `OBJECT IDENTIFIER` ASN.1 data type. Use an
+/// `OBJECT IDENTIFIER` when a compact numerical identification of a node of the
+/// OID tree is needed in binary encodings.
+/// # Example
+/// ```asn1
+/// -- NumericString ASN.1 type (see X.680 41.3) --
+/// numericString OBJECT IDENTIFIER ::=
+/// { joint-iso-itu-t asn1(1) specification(0) characterStrings(1) numericString(0) }
+///
+/// -- PrintableString ASN.1 type (see X.680 41.5) --
+/// printableString OBJECT IDENTIFIER ::=
+/// { joint-iso-itu-t asn1(1) specification(0) characterStrings(1) printableString(1) }
+///
+/// -- BER encoding of a single ASN.1 type --
+/// ber OBJECT IDENTIFIER ::=
+/// { joint-iso-itu-t asn1(1) basic-encoding(1) }
+///
+/// -- CER encoding of a single ASN.1 type --
+/// cer OBJECT IDENTIFIER ::=
+/// { joint-iso-itu-t asn1(1) ber-derived(2) canonical-encoding(0) }
+///
+/// -- DER encoding of a single ASN.1 type --
+/// der OBJECT IDENTIFIER ::=
+/// { joint-iso-itu-t asn1(1) ber-derived(2) distinguished-encoding(1) }
+/// ```
 #[derive(Clone, Debug, Deserialize, Eq, Hash, PartialEq, Serialize)]
 #[serde(rename = "ASN.1#ObjectIdentifier")]
 pub struct ObjectIdentifier<T: AsRef<[u128]>>(pub T);
