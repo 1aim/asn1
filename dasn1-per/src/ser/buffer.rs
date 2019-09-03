baa86efd74c78c82f07998f3c0c66a1e3c7808a7
Erin Power <xampprocky@gmail.com>
2019-09-03 07:39:14 +0200

    Fixed warning

--- dasn1-per/src/ser/buffer.rs
+++ dasn1-per/src/ser/buffer.rs
@@ -12,7 +12,7 @@ impl Buffer {
         Self(BitVec::from_elem(n, default))
     }
 
-    pub fn push_field_list(&mut self, mut target: Self) {
+    pub fn push_field_list(&mut self, target: Self) {
         for bit in target.into_inner() {
             self.0.push(bit);
         }
@@ -34,20 +34,5 @@ impl std::ops::Deref for Buffer {
 impl std::ops::DerefMut for Buffer {
     fn deref_mut(&mut self) -> &mut Self::Target {
         &mut self.0
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn push_to_field_list() {
-        let mut a = Buffer::from_elem(3, false);
-        let mut b = Buffer::from_elem(4, false);
-
-        a.append(&mut b);
-
-        assert_eq!(7, a.len());
     }
 }
